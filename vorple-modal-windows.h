0cd2b782a8bcd2bef355b186e3e71269418fc240
hugo <hugo@hlabrande.fr>
2018-10-21 18:03:33 +0200

    Modal Windows: option to wait for a key (mimic d28fafa of the i7 repo)

--- vorple-modal-windows.h
+++ vorple-modal-windows.h
@@ -7,20 +7,18 @@ Include "vorple.h";
 
 ! Modal windows are dialog prompts or other information windows that pop up on top of the play area and require user action to dismiss.
 
+Constant VORPLE_MODAL_WINDOW_PAUSE = 1;
 
-[ VorpleShowModalWindow text ;
+[ VorpleShowModalWindow text pausing ;
     if (text == 0) { text = ""; }
     if (isVorpleSupported()) {
         VorpleExecuteJavaScriptCommand(BuildCommand("vex.closeAll();vex.dialog.open({message:'", VorpleEscapeLineBreaks(text, "@@92n"), "',buttons:[vex.dialog.buttons.YES],callback:vorple.layout.unblock});vorple.layout.block()"));
     } else { print (string) text; }
+    if (pausing == VORPLE_MODAL_WINDOW_PAUSE) { KeyCharPrimitive(); }
 ];
 
 [ VorpleSetFocusToModalWindow ;
     VorpleSetOutputFocus("vex-dialog-message");        ! defined in vorple.h
-];
-
-[ VorpleCloseModalWindow ;
-    VorpleExecuteJavaScriptCommand("vex.closeAll()");
 ];
 
 #Endif;
