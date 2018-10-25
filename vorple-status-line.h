9a30abfe7d3591040c6fe4a737d816b6640da1cf
hugo <hugo@hlabrande.fr>
2018-10-25 22:05:36 +0200

    Status Line extension (mimics commit 2ed9fee and the other part of 0ae8c7f of the i7 repo)

---
+++ vorple-status-line.h
@@ -0,0 +1,149 @@
+#Ifndef VORPLE_STATUS_LINE;
+
+System_file;
+Constant VORPLE_HYPERLINKS;
+
+Include "vorple.h";
+Include "vorple-screen-effects.h";
+Include "vorple-element-manipulation.h";
+
+! Comment this out to not get a status line with full width
+Constant VORPLE_STATUS_LINE_FULL_WIDTH;
+! Don't change this number directly - internal use only
+Global VorpleStatusLineSize = 0;
+
+!===============================
+! Construct the Vorple status line with a given number of columns
+
+[ VorpleConstructStatusLine cols ;
+    if (cols > 3 || cols < 0) {
+        VorpleRuntimeError("Vorple Status Line: status line must have exactly 1, 2 or 3 columns.");
+        return false
+    }
+    VorpleStatusLineSize = cols;
+    VorpleStatusLineRemove();
+    VorplePlaceElementAtTopLevel("status-line-container");
+    #IfDef VORPLE_STATUS_LINE_FULL_WIDTH;
+    VorpleExecuteJavaScriptCommand("$('.status-line-container').prependTo('main#haven')");
+    #Ifnot;
+    VorpleExecuteJavaScriptCommand("$('.status-line-container').prependTo('#output')");
+    #Endif;
+    VorpleSetOutputFocus("status-line-container");
+    if (cols > 1) {
+        VorplePlaceBlockLevelElement("status-line-left col-xs lg-only");
+    }
+    if (cols ~=2) {
+        VorplePlaceBlockLevelElement("status-line-middle col-xs lg-only");
+    }
+    if (cols > 1) {
+        VorplePlaceBlockLevelElement("status-line-right col-xs lg-only");
+    }
+    VorplePlaceBlockLevelElement("status-line-mobile col-xs sm-only");
+    VorpleSetOutputFocusMainWindow();
+];
+
+
+!===============================
+! Drawing the usual status line
+
+
+Constant LEN_STATUS 200;
+Array VorpleStatusLineLeftHand buffer LEN_STATUS;
+Array VorpleStatusLineMiddleHand buffer LEN_STATUS;
+Array VorpleStatusLineRightHand buffer LEN_STATUS;
+Array VorpleStatusLineMobile buffer LEN_STATUS;
+! Put this variable at 0 if you don't want those to be updated with the usual location/score text
+Constant VORPLE_USUAL_STATUS_LINE = 1;
+
+
+Object VorpleDrawStatusLine "" VorpleInterfaceConstruction
+    with description [ r ;
+        VorpleConstructStatusLine();
+        if (VORPLE_USUAL_STATUS_LINE == 1) {
+            ! update the columns text using the usual DrawStatusLine
+            VorpleStatusLineUsualInfo();
+        }
+        if (child(StatusLineRulebook) == nothing) {
+            VorpleDefaultStatusLine.description();
+        } else {
+            objectloop(r in StatusLineRulebook) { r.description(); }
+        }
+        return true;
+    ];
+
+
+Object StatusLineRulebook "status line rulebook";
+
+Object VorpleDefaultStatusLine
+    with description [;
+        if (VorpleStatusLineSize > 1) {
+            VorplePutContentInAllElements("status-line-left", VorpleStatusLineLeftHand);
+        }
+        if (VorpleStatusLineSize == 2) {
+            VorplePutContentInAllElements("status-line-middle", VorpleStatusLineMiddleHand);
+        }
+        if (VorpleStatusLineSize > 1) {
+            VorplePutContentInAllElements("status-line-right", VorpleStatusLineRightHand);
+        }
+        VorplePutContentInAllElements("status-line-mobile", VorpleStatusLineMobile);
+        return ;
+    ];
+
+
+! Update the columns text using the usual DrawStatusLine code
+[ VorpleStatusLineUsualInfo ;
+    bp_output_stream(3, VorpleStatusLineLeftHand, LEN_STATUS);
+    if (location == thedark) {
+        print (name) location;
+    } else {
+        FindVisibilityLevels();
+        if (visibility_ceiling == location)
+            print (name) location;
+        else
+            print (The) visibility_ceiling;
+    }
+    bp_output_stream(-3);
+    
+    bp_output_stream(3, VorpleStatusLineRightHand, LEN_STATUS);
+        if (sys_statusline_flag && width > 53) {
+            print (string) TIME__TX;
+            LanguageTimeOfDay(sline1, sline2);
+        } else {
+            if (width > 66) {
+                #Ifndef NO_SCORE;
+                print (string) SCORE__TX, sline1;
+                #Endif;
+                print "   ";
+                print (string) MOVES__TX, sline2;
+            }
+            #Ifndef NO_SCORE;
+            if (width > 53 && width <= 66) {
+                print sline1, "/", sline2;
+            }
+            #Endif;
+        }
+        bp_output_stream(-3);
+];
+    
+!===============================
+! Refresh, remove, clear
+    
+
+[ VorpleStatusLineRefresh ;
+    if (isVorpleSupported() && VorpleStatusLineSize > 0) {
+        ! save the internal state of line breaks
+        VorpleDrawStatusLine();
+        
+        ! restore the internal state of line breaks
+    }
+];
+
+[ VorpleStatusLineRemove ;
+    VorpleRemoveElement("status-line-container");
+];
+
+[ VorpleStatusLineClear ;
+    VorpleExecuteJavaScriptCommand("$('.status-line-container').children().empty()");
+];
+
+#Endif;
